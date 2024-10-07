417ed5d2a9ede7b89b3e3d833af23b024ed62851
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-10-07 05:16:45 -0500

    Remove test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from openalex_taxicab.harvest import PublisherLandingPageHarvester
-from openalex_taxicab.s3_util import make_s3
-
-s3 = make_s3()
-
-harvester = PublisherLandingPageHarvester(s3)
-response = harvester.harvest('10.3389/fnins.2021.765850', 'Frontiers Media SA')
-print(response)
