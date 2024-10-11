3c8d46a72ad57b93fa5377bf497f5350c5992af2
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-10-10 20:35:45 -0500

    Fix requirements parsing

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 from setuptools import setup
 
+def parse_requirements(filename):
+    with open(filename) as f:
+        requirements = []
+        for line in f:
+            line = line.strip()
+            if line and not line.startswith('#'):
+                if line.startswith('git+'):
+                    pkg_name = line.split('#egg=')[-1]
+                    requirements.append(f'{pkg_name} @ {line}')
+                else:
+                    requirements.append(line)
+    return requirements
+
 setup(
     name='openalex-taxicab',
     version='0.1',
     packages=['openalex_taxicab'],
     url='https://github.com/ourresearch/openalex-taxicab',
-    install_requires=[line for line in open('./requirements.txt').read().splitlines() if len(line) > 3],
+    install_requires=parse_requirements('./requirements.txt'),
     license='',
     author='',
     author_email='',
