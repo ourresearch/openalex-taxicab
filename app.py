efec5d17ed1516568ab41fea78d81c24180b9afc
Casey M <caseym@gmail.com>
2025-01-30 20:40:01 -0600

    docker ready

--- app.py
+++ app.py
@@ -49,5 +49,5 @@ def harvest_content():
         'native_id_namespace': result['native_id_namespace']
     })
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=8080, debug=True)
