33078f288575b5a019e559e7b58b20e6300ebdfe
Casey M <caseym@gmail.com>
2025-02-01 15:42:06 -0600

    change endpoints to taxicab, add get file endpoint

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify
+import gzip
+
+from flask import Flask, request, jsonify, Response
 import boto3
 
 from openalex_taxicab.harvest import Harvester
@@ -16,7 +18,7 @@ def index():
     })
 
 
-@app.route("/harvest", methods=['POST'])
+@app.route("/taxicab", methods=['POST'])
 def harvest_content():
     data = request.get_json()
 
@@ -48,6 +50,50 @@ def harvest_content():
         'native_id': result['native_id'],
         'native_id_namespace': result['native_id_namespace']
     })
+
+
+@app.route("/taxicab/<uuid:harvest_id>", methods=['GET'])
+def fetch_harvested_content(harvest_id):
+    harvest_id = str(harvest_id)
+    possible_buckets = [harvester.HTML_BUCKET, harvester.PDF_BUCKET]
+
+    for bucket in possible_buckets:
+        s3_key_html = f"{harvest_id}.html.gz"
+        s3_key_pdf = f"{harvest_id}.pdf"
+
+        try:
+            # try HTML (gzipped)
+            obj = s3_client.get_object(Bucket=bucket, Key=s3_key_html)
+            content = gzip.decompress(obj['Body'].read())
+            content_type = "text/html"
+            return Response(
+                content,
+                content_type=content_type,
+                headers={"Content-Disposition": f"inline; filename={harvest_id}.html"}
+            )
+
+        except s3_client.exceptions.NoSuchKey:
+            pass  # continue to check for PDF
+        except Exception as e:
+            return jsonify({"error": f"Error fetching HTML: {str(e)}"}), 500
+
+        try:
+            # try PDF
+            obj = s3_client.get_object(Bucket=bucket, Key=s3_key_pdf)
+            content = obj['Body'].read()
+            content_type = "application/pdf"
+            return Response(
+                content,
+                content_type=content_type,
+                headers={"Content-Disposition": f"attachment; filename={harvest_id}.pdf"}
+            )
+
+        except s3_client.exceptions.NoSuchKey:
+            pass  # if PDF is not found, try the next bucket
+        except Exception as e:
+            return jsonify({"error": f"Error fetching PDF: {str(e)}"}), 500
+
+    return jsonify({"error": "File not found"}), 404
 
 # if __name__ == "__main__":
 #     app.run(host='0.0.0.0', port=8080, debug=True)
